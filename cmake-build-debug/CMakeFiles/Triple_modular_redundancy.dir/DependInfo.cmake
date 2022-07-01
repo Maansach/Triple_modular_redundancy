1e71d32f8b22ef26e5e9ffb164c23c1222aa2ab5
Maansach <maanasa.sachi1997@gmail.com>
2022-07-01 14:15:28 +0200

    First commit

---
+++ cmake-build-debug/CMakeFiles/Triple_modular_redundancy.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/maana/CLionProjects/Triple_modular_redundancy/main.c" "C:/Users/maana/CLionProjects/Triple_modular_redundancy/cmake-build-debug/CMakeFiles/Triple_modular_redundancy.dir/main.c.obj"
+  "C:/Users/maana/CLionProjects/Triple_modular_redundancy/pipe.c" "C:/Users/maana/CLionProjects/Triple_modular_redundancy/cmake-build-debug/CMakeFiles/Triple_modular_redundancy.dir/pipe.c.obj"
+  "C:/Users/maana/CLionProjects/Triple_modular_redundancy/sink.c" "C:/Users/maana/CLionProjects/Triple_modular_redundancy/cmake-build-debug/CMakeFiles/Triple_modular_redundancy.dir/sink.c.obj"
+  "C:/Users/maana/CLionProjects/Triple_modular_redundancy/source.c" "C:/Users/maana/CLionProjects/Triple_modular_redundancy/cmake-build-debug/CMakeFiles/Triple_modular_redundancy.dir/source.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Triple_modular_redundancy.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Triple_modular_redundancy.dir/main.c.obj"
+  "CMakeFiles/Triple_modular_redundancy.dir/pipe.c.obj"
+  "CMakeFiles/Triple_modular_redundancy.dir/sink.c.obj"
+  "CMakeFiles/Triple_modular_redundancy.dir/source.c.obj"
+  "Triple_modular_redundancy.exe"
+  "Triple_modular_redundancy.exe.manifest"
+  "Triple_modular_redundancy.pdb"
+  "libTriple_modular_redundancy.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Triple_modular_redundancy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
