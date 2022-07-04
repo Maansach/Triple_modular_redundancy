4b47d54a6ef61094e4132a09162afefb674ccd92
Maansach <73380968+Maansach@users.noreply.github.com>
2022-07-04 14:24:06 +0200

    added description

--- source.c
+++ source.c
@@ -1,5 +1,6 @@
 //
 // Created by maana on 7/1/2022.
+// The source accepts an array of numbers and will return one number at each cycle and then move to the next number.
 //
 
 int generate_value_for_number_array(int *array, int index) {
